be69d6f13ef766e23e99d10ad5506f43839db0fd
Mark Bieganek <mark.bieganek@telus.com>
2023-01-26 22:09:29 -0700

    Add validation, format form with bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,16 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  before_save :downcase_email
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true
+  validates :password_confirmation, presence: true
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
 end
