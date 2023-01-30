8b3f1fc1cfa82f6ebe2eab30c3ce31d0a281fe9c
Mark Bieganek <mark.bieganek@telus.com>
2023-01-30 14:21:25 -0700

    Rewrite specs with context methods, for clarity

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,35 +7,45 @@ RSpec.describe Product, type: :model do
     @product = Product.new(name: 'Test Product Name', price: 23.21, quantity: 3, category: @category)
   end
 
-  describe "validations" do
-    it "should create a product when there are no validation errors" do
-      @product.save
-      expect(@product.errors).to be_empty
+  describe "validates" do
+    context "given valid product inputs" do
+      it "should create a product with no errors" do
+        @product.save
+        expect(@product.errors).to be_empty
+      end
     end
 
-    it "should not allow a product to be created without a name" do
-      @product.name = nil
-      @product.save
-      expect(@product.errors[:name]).to include("can't be blank")
+    context "given an empty name" do
+      it "should not allow a product to be created and return an error" do
+        @product.name = nil
+        @product.save
+        expect(@product.errors[:name]).to include("can't be blank")
+      end
     end
 
-    it "should not allow a product to be created without a price" do
-      @product.price = nil
-      @product.price_cents = nil
-      @product.save
-      expect(@product.errors[:price]).to include("can't be blank")
+    context "given an empty price" do
+      it "should not allow a product to be created and return an error" do
+        @product.price = nil
+        @product.price_cents = nil
+        @product.save
+        expect(@product.errors[:price]).to include("can't be blank")
+      end
     end
 
-    it "should not allow a product to be created without a quantity" do
-      @product.quantity = nil
-      @product.save
-      expect(@product.errors[:quantity]).to include("can't be blank")
+    context "given an empty quantity" do
+      it "should not allow a product to be created and return an error" do
+        @product.quantity = nil
+        @product.save
+        expect(@product.errors[:quantity]).to include("can't be blank")
+      end
     end
 
-    it "should not allow a product to be created without a category" do
-      @product.category = nil
-      @product.save
-      expect(@product.errors[:category]).to include("can't be blank")
+    context "given an empty category" do
+      it "should not allow a product to be created and return an error" do
+        @product.category = nil
+        @product.save
+        expect(@product.errors[:category]).to include("can't be blank")
+      end
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,46 +6,56 @@ RSpec.describe User, type: :model do
     @user = User.new(first_name: 'First', last_name: 'Last', email: 'test@email.com', password: '123Password', password_confirmation: '123Password')
   end
 
-  describe "validations" do
-    it "should create a user when there are no validation errors" do
-      @user.save
-      expect(@user.errors).to be_empty
+  describe "validates" do
+    context "given valid user inputs" do
+      it "should create a user with no errors" do
+        @user.save
+        expect(@user.errors).to be_empty
+      end
     end
     
-    describe "give an error if required input is not provided" do
-      it "should not allow a user to be created without a first name" do
+    context "given an empty first_name" do
+      it "should not allow a user to be created and return an error" do
         @user.first_name = nil
         @user.save
         expect(@user.errors[:first_name]).to include("can't be blank")
       end
+    end
       
-      it "should not allow a user to be created without a last name" do
+    context "given an empty last_name" do
+      it "should not allow a user to be created and return an error" do
         @user.last_name = nil
         @user.save
         expect(@user.errors[:last_name]).to include("can't be blank")
       end
+    end
 
-      it "should not allow a user to be created without an email" do
+    context "given an empty email" do
+      it "should not allow a user to be created and return an error" do
         @user.email = nil
         @user.save
         expect(@user.errors[:email]).to include("can't be blank")
       end
+    end
 
-      it "should not allow a user to be created without a password" do
+    context "given an empty email" do
+      it "should not allow a user to be created and return an error" do
         @user.password = nil
         @user.save
         expect(@user.errors[:password]).to include("can't be blank")
       end
+    end
 
-      it "should not allow a user to be created without a password_confirmation" do
+    context "given an empty password_confirmation" do
+      it "should not allow a user to be created and return an error" do
         @user.password_confirmation = nil
         @user.save
         expect(@user.errors[:password_confirmation]).to include("can't be blank")
       end
     end
 
-    describe "give an error if passwords do not match" do
-      it "should not allow a user enter a different password in password_confirmation" do
+    context "given a mismatched password_confirmation" do
+      it "should not allow a user to be created and return an error" do
         @user.password_confirmation = "321password"
         @user.save
         expect(@user.errors[:password_confirmation]).to include("doesn't match Password")
