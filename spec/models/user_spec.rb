f8a3c580138d41a0c505be53ae56cebc3a3ca6ee
Mark Bieganek <mark.bieganek@telus.com>
2023-01-30 14:08:23 -0700

    Test User create with mismatched passwords

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,35 +11,45 @@ RSpec.describe User, type: :model do
       @user.save
       expect(@user.errors).to be_empty
     end
-
-    it "should not allow a user to be created without a first name" do
-      @user.first_name = nil
-      @user.save
-      expect(@user.errors[:first_name]).to include("can't be blank")
-    end
     
-    it "should not allow a user to be created without a last name" do
-      @user.last_name = nil
-      @user.save
-      expect(@user.errors[:last_name]).to include("can't be blank")
+    describe "give an error if required input is not provided" do
+      it "should not allow a user to be created without a first name" do
+        @user.first_name = nil
+        @user.save
+        expect(@user.errors[:first_name]).to include("can't be blank")
+      end
+      
+      it "should not allow a user to be created without a last name" do
+        @user.last_name = nil
+        @user.save
+        expect(@user.errors[:last_name]).to include("can't be blank")
+      end
+
+      it "should not allow a user to be created without an email" do
+        @user.email = nil
+        @user.save
+        expect(@user.errors[:email]).to include("can't be blank")
+      end
+
+      it "should not allow a user to be created without a password" do
+        @user.password = nil
+        @user.save
+        expect(@user.errors[:password]).to include("can't be blank")
+      end
+
+      it "should not allow a user to be created without a password_confirmation" do
+        @user.password_confirmation = nil
+        @user.save
+        expect(@user.errors[:password_confirmation]).to include("can't be blank")
+      end
     end
 
-    it "should not allow a user to be created without an email" do
-      @user.email = nil
-      @user.save
-      expect(@user.errors[:email]).to include("can't be blank")
-    end
-
-    it "should not allow a user to be created without a password" do
-      @user.password = nil
-      @user.save
-      expect(@user.errors[:password]).to include("can't be blank")
-    end
-
-    it "should not allow a user to be created without a password_confirmation" do
-      @user.password_confirmation = nil
-      @user.save
-      expect(@user.errors[:password_confirmation]).to include("can't be blank")
+    describe "give an error if passwords do not match" do
+      it "should not allow a user enter a different password in password_confirmation" do
+        @user.password_confirmation = "321password"
+        @user.save
+        expect(@user.errors[:password_confirmation]).to include("doesn't match Password")
+      end
     end
 
   end
